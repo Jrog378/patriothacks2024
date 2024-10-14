ffac66f6a8228f1e038e90ffb0be4f8934e6cd0c
Justin Rogers <Jayjust378@gmail.com>
2024-10-14 10:17:08 -0400

    Final Push Website Post-Submission

--- src/pages/procedure.js
+++ src/pages/procedure.js
@@ -13,21 +13,38 @@ export default function Procedure() {
                         </h1>
                     </Row>
                     <Row>
-                        <Col style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
-                            <h2>Thank You For Your Time and Consideration</h2>
-                            <p>
-                                We appreciate your attendance in taking a look at our project. I hope that you enjoy the
-                                journey through seeing the results as much as we enjoyed making them.
-                            </p>
+                        <Col xs={12} md={6}>
+                            <Container style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
+                                <Container style={{padding: '5%'}}>
+
+
+                                    <h2>Thank You For Your Time and Consideration</h2>
+                                    <p>
+                                        We appreciate your attendance in taking a look at our project. I hope that you
+                                        enjoy the
+                                        journey through seeing the results as much as we enjoyed making them. Our target
+                                        tracks
+                                        are "Save the World", "Best React App", and "AWS/MetroStar National Treasure
+                                        Theft."
+                                    </p>
+                                </Container>
+                            </Container>
                         </Col>
-                        <Col style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
-                            <h2>Ready to Solve a Mystery?</h2>
-                            <p>
-                                Today we start off by observing scenes from surveillance video taken at the scene of a
-                                robbery of an ancient artifact called The Four Goat Square Zun. We only have limited
-                                images of the target vehicles, and it's our job to create a reorganization model to fix
-                                that.
-                            </p>
+                        <Col xs={12} md={6}>
+                            <Container style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
+                                <Container style={{padding: '5%'}}>
+                                    <h2>Ready to Solve a Mystery?</h2>
+                                    <p>
+                                        Today we start off by observing scenes from surveillance video taken at the
+                                        scene of a
+                                        robbery of an ancient artifact called The Four Goat Square Zun. We only have
+                                        limited
+                                        images of the target vehicles, and it's our job to create a reorganization model
+                                        to fix
+                                        that.
+                                    </p>
+                                </Container>
+                            </Container>
                         </Col>
                     </Row>
                     <Row>
@@ -51,25 +68,36 @@ export default function Procedure() {
                         </h1>
                     </Row>
                     <Row>
-                        <Col style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
-                            <h2>
-                                AWS Rekognition & More
-                            </h2>
-                            <p>
-                                In our project we primarily used AWS Recognition to analyze our photos and work with our
-                                Machine Learning models. On top of AWS, we used assistance form programming languages
-                                like Python when the problems got more difficult.
-                            </p>
+                        <Col xs={12} md={6}>
+                            <Container style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
+                                <Container style={{padding: '5%'}}>
+                                    <h2>
+                                        AWS Rekognition & More
+                                    </h2>
+                                    <p>
+                                        In our project we primarily used AWS Recognition to analyze our photos and work
+                                        with our
+                                        Machine Learning models. On top of AWS, we used assistance form programming
+                                        languages
+                                        like Python when the problems got more difficult.
+                                    </p>
+                                </Container>
+                            </Container>
                         </Col>
-                        <Col style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
-                            <h2>
-                                Feature Engineering & Data
-                            </h2>
-                            <p>
-                                Our original dataset was stored in buckets with S3, provided by AWS for easy integration
-                                into Rekognition, where we trained and tested our models drawing boxes on target
-                                entities. We used packages like OpenCV and Numpy.
-                            </p>
+                        <Col xs={12} md={6}>
+                            <Container style={{background: 'rgb(225, 225, 235)', borderRadius: '25px', margin: '10px'}}>
+                                <Container style={{padding: '5%'}}>
+                                    <h2>
+                                        Feature Engineering & Data
+                                    </h2>
+                                    <p>
+                                        Our original dataset was stored in buckets with S3, provided by AWS for easy
+                                        integration
+                                        into Rekognition, where we trained and tested our models drawing boxes on target
+                                        entities. We used packages like OpenCV and Numpy.
+                                    </p>
+                                </Container>
+                            </Container>
                         </Col>
                     </Row>
                     <Container style={{textAlign: 'center'}}>
--- src/pages/results.js
+++ src/pages/results.js
@@ -10,31 +10,57 @@ export default function Results() {
                     Results
                 </h1>
                 <Row>
-                    <Col style={{background:'rgb(225, 225, 235)', borderRadius:'25px', margin:'10px'}}>
-                        <h2>
-                            AWS Rekognition Results
-                        </h2>
-                        <p>
-                            AWS Rekognition was an amazing resource for our group to quickly build, train, and test our
-                            models, especially with the help of AWS staff on site and the SageMaker Classic IDE with
-                            it's built in tutorials. Our first model was able to obtain a F1 score of 0.933, a Average
-                            Precision of 1.0, and an Overall Recall of 0.9. Our second test obtained scores of 1.0s all
-                            across the board. Finally, our third test performed very poorly in comparison with an F1
-                            score of 0.11, an Average Precision of 0.07 and an Overall Recall of 0.3. Since we were
-                            having trouble with this last model, we decided to supplement with some hard code as well.
-                        </p>
+                    <Col xs={12} lg={6}>
+                        <Container style={{margin: '10px', background: 'rgb(225, 225, 235)', borderRadius: '25px'}}>
+                            <Container style={{padding: '5%'}}>
+                                <h2>
+                                    AWS Rekognition Results
+                                </h2>
+                                <p>
+                                    AWS Rekognition was an amazing resource for our group to quickly build, train, and
+                                    test
+                                    our
+                                    models, especially with the help of AWS staff on site and the SageMaker Classic IDE
+                                    with
+                                    it's built in tutorials. Our first model was able to obtain a <strong>F1 score of
+                                    0.933,
+                                    an
+                                    Average Precision of 1.0(100% Accuracy), and an Overall Recall of 0.9</strong> in
+                                    our
+                                    initial testing. Our second test obtained <strong>scores of all
+                                    1.0s(100%)</strong> across
+                                    the board. Finally, our third test performed very poorly in comparison with an F1
+                                    score
+                                    of
+                                    0.11, an Average Precision of 0.07 and an Overall Recall of 0.3. Since we were
+                                    having
+                                    trouble with this last model, we decided to supplement with some hard code as well.
+                                </p>
+                            </Container>
+                        </Container>
                     </Col>
-                    <Col style={{background:'rgb(225, 225, 235)', borderRadius:'25px', margin:'10px'}}>
-                        <h2>
-                            Hard Coded Results
-                        </h2>
-                        <p>
-                            We attempted to hard code the third model in both Python and Java. Our Python Model was able
-                            to obtain an accuracy of around 80% with 15 True Positives, 10 True Negatives, 0 False
-                            Positives, and 6 False Negatives. We are relatively happy with these results because rather
-                            than catching the wrong car in our model and wasting resources to look into those photos, it
-                            would be more efficient to just skip over some that could potentially be suspects.
-                        </p>
+                    <Col xs={12} lg={6}>
+                        <Container style={{margin: '10px', background: 'rgb(225, 225, 235)', borderRadius: '25px'}}>
+                            <Container style={{padding: '5%'}}>
+                                <h2>
+                                    Hard Coded Results
+                                </h2>
+                                <p>
+                                    We attempted to hard code the third model in both Python and Java. Our Python Model
+                                    was
+                                    able
+                                    to obtain an <strong>accuracy of around 80% with 15 True Positives, 10 True
+                                    Negatives, 0
+                                    False
+                                    Positives, and 6 False Negatives</strong>. We are relatively happy with these
+                                    results
+                                    because rather
+                                    than catching the wrong car in our model and wasting resources to look into those
+                                    photos, it
+                                    would be more efficient to just skip over some that could potentially be suspects.
+                                </p>
+                            </Container>
+                        </Container>
                     </Col>
                 </Row>
                 <Container style={{textAlign: 'center'}}>
@@ -43,18 +69,18 @@ export default function Results() {
                     </h2>
                     <Row>
                         <Col xs={12} lg={4}>
-                            <Container style={{paddingBottom:'10%'}}>
-                                <Image style={{maxWidth:'80%'}} src={'Model1.png'}/>
+                            <Container style={{paddingBottom: '10%'}}>
+                                <Image style={{maxWidth: '80%'}} src={'Model1.png'}/>
                             </Container>
                         </Col>
                         <Col xs={12} lg={4}>
-                            <Container style={{paddingBottom:'10%'}}>
-                                <Image style={{maxWidth:'80%'}} src={'Model2.png'}/>
+                            <Container style={{paddingBottom: '10%'}}>
+                                <Image style={{maxWidth: '80%'}} src={'Model2.png'}/>
                             </Container>
                         </Col>
                         <Col xs={12} lg={4}>
-                            <Container style={{paddingBottom:'10%'}}>
-                                <Image style={{maxWidth:'80%'}} src={'Model3.png'}/>
+                            <Container style={{paddingBottom: '10%'}}>
+                                <Image style={{maxWidth: '80%'}} src={'Model3.png'}/>
                             </Container>
                         </Col>
                     </Row>
@@ -65,18 +91,18 @@ export default function Results() {
                     </h2>
                     <Row>
                         <Col xs={12} lg={4}>
-                            <Container style={{paddingBottom:'10%'}}>
-                                <Image style={{maxWidth:'80%'}} src={'Python1.png'}/>
+                            <Container style={{paddingBottom: '10%'}}>
+                                <Image style={{maxWidth: '80%'}} src={'Python1.png'}/>
                             </Container>
                         </Col>
                         <Col xs={12} lg={4}>
-                            <Container style={{paddingBottom:'10%'}}>
-                                <Image style={{maxWidth:'80%'}} src={'Python2.png'}/>
+                            <Container style={{paddingBottom: '10%'}}>
+                                <Image style={{maxWidth: '80%'}} src={'Python2.png'}/>
                             </Container>
                         </Col>
                         <Col xs={12} lg={4}>
-                            <Container style={{paddingBottom:'10%'}}>
-                                <Image style={{maxWidth:'80%'}} src={'Python3.png'}/>
+                            <Container style={{paddingBottom: '10%'}}>
+                                <Image style={{maxWidth: '80%'}} src={'Python3.png'}/>
                             </Container>
                         </Col>
                     </Row>
